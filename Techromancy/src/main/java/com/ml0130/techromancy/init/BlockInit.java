3f453a98ef084764c4fe9730507916079f095e03
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-01 15:09:44 -0400

    Update BlockInit.java

--- Techromancy/src/main/java/com/ml0130/techromancy/init/BlockInit.java
+++ Techromancy/src/main/java/com/ml0130/techromancy/init/BlockInit.java
@@ -1,33 +1,35 @@
 package com.ml0130.techromancy.init;
 
 import java.util.function.Function;
-import com.google.common.base.Supplier;
 
+import com.google.common.base.Supplier;
 import com.ml0130.techromancy.Techromancy;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class BlockInit {
-	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Techromancy.MOD_ID);
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
+			Techromancy.MOD_ID);
 	public static final DeferredRegister<Item> ITEMS = ItemInit.ITEMS;
 	
 	public static final RegistryObject<Block> Hex_Gate = register("hex_gate", 
-			() -> new Block(Block.Behaviour.of(Material.METAL, 	MaterialColor)), null);
+			() -> new Block(BlockBehaviour.OffsetType
 	
-	private static <T extends Block> RegistryObject<T> registerBlock(final String name, final Supplier<? extends T> block){
+	private static <T extends Block> RegistryObject<T> registerBlock(final String name,
+			final Supplier<? extends T> block) {
 		return BLOCKS.register(name, block);
-	
 	}
+
 	private static <T extends Block> RegistryObject<T> register(final String name, final Supplier<? extends T> block,
-				Function<RegistryObject<T>, Supplier<? extends T>> item){
+			Function<RegistryObject<T>, Supplier<? extends Item>> item) {
 		RegistryObject<T> obj = registerBlock(name, block);
 		ITEMS.register(name, item.apply(obj));
 		return obj;
-	}
 	
 }
