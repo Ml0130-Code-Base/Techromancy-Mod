d375948f69c4b6e93afc6ba0c0e1662996a14af5
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-02 12:29:08 -0400

    Fixed Sword naming

--- Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/solidified_mana_sward.java
+++
@@ -1,7 +0,0 @@
-package com.ml0130.techromancy.itemdata.tools;
-
-import net.minecraft.world.item.SwordItem;
-
-public class solidified_mana_sward {
-
-}
---
+++ Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/solidified_mana_sword.java
@@ -0,0 +1,13 @@
+package com.ml0130.techromancy.itemdata.tools;
+
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+
+public class solidified_mana_sword extends SwordItem{
+
+	public solidified_mana_sword(Tier p_43269_, int p_43270_, float p_43271_, Properties p_43272_) {
+		super(p_43269_, p_43270_, p_43271_, p_43272_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
