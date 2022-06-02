a3460aa3fca61a90c6985ee1864d089d699636eb
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-02 14:59:36 -0400

    Update ItemInit.java
    added  durabilty and rarity to solidified tools

--- Techromancy/src/main/java/com/ml0130/techromancy/init/ItemInit.java
+++ Techromancy/src/main/java/com/ml0130/techromancy/init/ItemInit.java
@@ -2,30 +2,31 @@ package com.ml0130.techromancy.init;
 
 import com.google.common.base.Supplier;
 import com.ml0130.techromancy.Techromancy;
-import com.ml0130.techromancy.itemdata.tools.solidifed_mana_hoe;
-
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Techromancy.MOD_ID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+			Techromancy.MOD_ID);
 
 	public static final RegistryObject<Item> Solidified_Mana = register("solidified_mana",
 			() -> new Item(new Item.Properties().tab(Techromancy.Techromancy_Tab)));
-	
-	//Tool Registry
+
+	// Tool Registry
 	public static final RegistryObject<Item> Solidified_Mana_Pickaxe = register("solidified_mana_pickaxe",
-			() -> new Item(new Item.Properties().tab(Techromancy.Techromancy_Tab)));
+			() -> new Item(new Item.Properties().tab(Techromancy.Techromancy_Tab).durability(5).rarity(Rarity.COMMON)));
 	public static final RegistryObject<Item> Solidified_Mana_Axe = register("solidified_mana_axe",
-			() -> new Item(new Item.Properties().tab(Techromancy.Techromancy_Tab)));
+			() -> new Item(new Item.Properties().tab(Techromancy.Techromancy_Tab).durability(5).rarity(Rarity.COMMON)));
 	public static final RegistryObject<Item> Solidified_Mana_Sword = register("solidified_mana_sword",
-			() -> new Item(new Item.Properties().tab(Techromancy.Techromancy_Tab)));
+			() -> new Item(new Item.Properties().tab(Techromancy.Techromancy_Tab).durability(5).rarity(Rarity.COMMON)));
 	public static final RegistryObject<Item> Solidified_Mana_Hoe = register("solidified_mana_hoe",
-			() -> new solidifed_mana_hoe(Common, 0, 0, new Item.Properties().tab(Techromancy.Techromancy_Tab)));
-	
-	private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item){
+			() -> new Item(new Item.Properties().tab(Techromancy.Techromancy_Tab).durability(5).rarity(Rarity.COMMON)));
+
+	private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item) {
 		return ITEMS.register(name, item);
 	}
 }
