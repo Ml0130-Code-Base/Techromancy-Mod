8ba89b5bf57f3f95cb0d803b8e2cc445fff685e3
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-02 15:00:27 -0400

    Update solidified_mana_pickaxe.java

--- Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/solidified_mana_pickaxe.java
+++ Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/solidified_mana_pickaxe.java
@@ -1,13 +1,15 @@
 package com.ml0130.techromancy.itemdata.tools;
 
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tier;
 
-public class solidified_mana_pickaxe extends PickaxeItem{
+public class solidified_mana_pickaxe extends PickaxeItem {
 
 	public solidified_mana_pickaxe(Tier p_42961_, int p_42962_, float p_42963_, Properties p_42964_) {
 		super(p_42961_, p_42962_, p_42963_, p_42964_);
 		// TODO Auto-generated constructor stub
 	}
 
+	public static final ToolType PICKAXE;
 }
