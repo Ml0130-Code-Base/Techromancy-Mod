5038f8fe279c74e39c99f4d51ceccc53872e616c
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-02 15:00:22 -0400

    Disableed Mana Hoe

--- Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/solidifed_mana_hoe.java
+++ Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/solidifed_mana_hoe.java
@@ -6,9 +6,8 @@ import net.minecraft.world.item.Tier;
 
 public class solidifed_mana_hoe extends HoeItem{
 
-	public solidifed_mana_hoe(Tier p_41336_, int p_41337_, float p_41338_, Properties p_41339_) {
-		super(p_41336_, p_41337_, p_41338_, p_41339_);
+	public solidifed_mana_hoe(Tier common, int p_41337_, float p_41338_, Properties p_41339_) {
+		super(common, p_41337_, p_41338_, p_41339_);
 		// TODO Auto-generated constructor stub
 	}
-	
 }
