221a6d9e9b67e1c93c252c232bc1f3f0f04a266e
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-03 10:58:56 -0400

    Update TechromancyDataGeneration.java

--- Techromancy/src/main/java/com/ml0130/techromancy/datagen/TechromancyDataGeneration.java
+++ Techromancy/src/main/java/com/ml0130/techromancy/datagen/TechromancyDataGeneration.java
@@ -2,6 +2,8 @@ package com.ml0130.techromancy.datagen;
 
 import com.ml0130.techromancy.Techromancy;
 import com.ml0130.techromancy.datagen.client.ModBlockStateProvider;
+import com.ml0130.techromancy.datagen.client.ModItemModelProvider;
+import com.ml0130.techromancy.datagen.client.lang.ModEnUsProvider;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -20,6 +22,8 @@ public class TechromancyDataGeneration {
 		
 			if (event.includeClient()) {
 				generator.addProvider(new ModBlockStateProvider(generator, helper));
+				generator.addProvider(new ModItemModelProvider(generator, helper));
+				generator.addProvider(new ModEnUsProvider(generator));
 				
 			}
 			if (event.includeServer()) {
