76c5aa1d9b91620b4f71af7bb90a82c5c2934685
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-03 10:36:49 -0400

    Create ModRecipeProvider.java

---
+++ Techromancy/src/main/java/com/ml0130/techromancy/datagen/server/ModRecipeProvider.java
@@ -0,0 +1,23 @@
+package com.ml0130.techromancy.datagen.server;
+
+import java.util.function.Consumer;
+
+import com.ml0130.techromancy.init.ItemInit;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+
+public class ModRecipeProvider extends RecipeProvider{
+
+	public ModRecipeProvider(DataGenerator generator) {
+		super(generator);
+		// TODO Auto-generated constructor stub
+	}
+	//@Override
+	//protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+		//ShapedRecipeBuilder.shaped(ItemInit.Solidified_Mana.get())
+			//.define(null, null);
+	//}
+}
