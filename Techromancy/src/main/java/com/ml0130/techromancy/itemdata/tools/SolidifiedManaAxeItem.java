6042269d87da2106b930ec3910c2743c14ecfbaf
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-05 17:16:12 -0700

    Replaced

--- Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/SolidifiedManaAxeItem.java
+++ Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/SolidifiedManaAxeItem.java
@@ -3,9 +3,9 @@ package com.ml0130.techromancy.itemdata.tools;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Tier;
 
-public class solidified_mana_axe extends AxeItem{
+public class SolidifiedManaAxeItem extends AxeItem{
 
-	public solidified_mana_axe(Tier p_40521_, float p_40522_, float p_40523_, Properties p_40524_) {
+	public SolidifiedManaAxeItem(Tier p_40521_, float p_40522_, float p_40523_, Properties p_40524_) {
 		super(p_40521_, p_40522_, p_40523_, p_40524_);
 		// TODO Auto-generated constructor stub
 	}
