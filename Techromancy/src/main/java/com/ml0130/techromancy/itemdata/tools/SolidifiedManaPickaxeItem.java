99ebe41ba5b4fa32814e71fae1da74a0e806b793
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-05 17:15:53 -0700

    Replayed

--- Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/SolidifiedManaPickaxeItem.java
+++ Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/SolidifiedManaPickaxeItem.java
@@ -3,9 +3,9 @@ package com.ml0130.techromancy.itemdata.tools;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tier;
 
-public class solidified_mana_pickaxe extends PickaxeItem{
+public class SolidifiedManaPickaxeItem extends PickaxeItem{
 
-	public solidified_mana_pickaxe(Tier p_42961_, int p_42962_, float p_42963_, Properties p_42964_) {
+	public SolidifiedManaPickaxeItem(Tier p_42961_, int p_42962_, float p_42963_, Properties p_42964_) {
 		super(p_42961_, p_42962_, p_42963_, p_42964_);
 		// TODO Auto-generated constructor stub
 	}
