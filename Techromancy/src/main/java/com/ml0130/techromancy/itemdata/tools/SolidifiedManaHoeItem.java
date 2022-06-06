49e3986d7957c71de30a17d517407f33998a489e
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-05 17:14:47 -0700

    Replaced

---
+++ Techromancy/src/main/java/com/ml0130/techromancy/itemdata/tools/SolidifiedManaHoeItem.java
@@ -0,0 +1,13 @@
+package com.ml0130.techromancy.itemdata.tools;
+
+import net.minecraft.world.item.HoeItem;
+import net.minecraft.world.item.Tier;
+
+public class SolidifiedManaHoeItem extends HoeItem{
+
+	public SolidifiedManaHoeItem(Tier p_41336_, int p_41337_, float p_41338_, Properties p_41339_) {
+		super(p_41336_, p_41337_, p_41338_, p_41339_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
