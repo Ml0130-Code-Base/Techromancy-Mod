ce57e5d846ab774873738058268bb824aa39dbac
MICHAEL LAROSH <vgml0130@gmail.com>
2022-06-21 00:29:50 -0700

    Update DiscoveryTable.java

--- Techromancy/src/main/java/com/ml0130/techromancy/block/reserch/DiscoveryTable.java
+++ Techromancy/src/main/java/com/ml0130/techromancy/block/reserch/DiscoveryTable.java
@@ -2,11 +2,13 @@ package com.ml0130.techromancy.block.reserch;
 
 import net.minecraft.world.level.block.Block;
 
-public class DiscoveryTable extends Block{
+
+public class DiscoveryTable	extends Block {
 
 	public DiscoveryTable(Properties properties) {
 		super(properties);
 		// TODO Auto-generated constructor stub
 	}
 
+		
 }
